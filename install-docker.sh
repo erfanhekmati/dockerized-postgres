691071a0ebf173a726ac93fecaa0f1b3b6fa6fa7
Erfan Hekmati <erfanhekmati8@gmail.com>
2024-05-31 14:52:00 +0330

    Install docker and docker-compose bash script added

---
+++ install-docker.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+install_docker_ubuntu() {
+    sudo apt update
+    sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+    echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt update
+    sudo apt install -y docker-ce docker-ce-cli containerd.io
+    sudo usermod -aG docker $USER
+    clear
+}
+
+install_docker_compose_ubuntu() {
+    DOCKER_COMPOSE_VERSION=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep -Po '"tag_name": "\K.*\d')
+    sudo curl -L "https://github.com/docker/compose/releases/download/$DOCKER_COMPOSE_VERSION/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+    docker-compose --version
+    clear
+}
+
+install_docker_ubuntu
+install_docker_compose_ubuntu
+echo "Docker and Docker Compose installed successfully!"
